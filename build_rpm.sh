a9adc7998ee4f7f381ea420cf200e22e10682e2c
Joris Nogneng <joris.nogneng@sap.com>
2018-06-01 17:33:53 -0700

    update rpm with added version and parameters

--- build_rpm.sh
+++ build_rpm.sh
@@ -4,15 +4,17 @@
 ant tar
 
 # rename target directory
-mv ./build/sqoop-1.4.7.bin__hadoop-2.6.0 ./build/sqoop-1.4.7
+mv ./build/sqoop-${SQOOP_VERSION}.bin__hadoop-2.6.0 ./build/sqoop-${SQOOP_VERSION}
 
 # delete shell scripts that have been copied in build
-rm ./build/sqoop-1.4.7/build_scoop.sh ./build/sqoop-1.4.7/build_rpm.sh ./build/sqoop-1.4.7/launch_docker_build_rpm.sh
+rm ./build/sqoop-${SQOOP_VERSION}/build_scoop.sh ./build/sqoop-${SQOOP_VERSION}/build_rpm.sh ./build/sqoop-${SQOOP_VERSION}/launch_docker_build_rpm.sh
 
 # delete rpm if exists
-if [ -f ./alti-sqoop-1.4.7-1.x86_64.rpm ]; then
-	rm ./alti-sqoop-1.4.7-1.x86_64.rpm
+if [ -f ./alti-sqoop-${SQOOP_VERSION}.x86_64.rpm ]; then
+	rm ./alti-sqoop-${SQOOP_VERSION}.x86_64.rpm
 fi
 
+export DATE_STRING=$(date "+%Y%m%d%H%M")
+
 # 2. build rpm
-fpm --verbose --maintainer support@altiscale.com --vendor Altiscale --provides alti-sqoop -n alti-sqoop -v 1.4.7 --license "Apache License v2" --after-install create_link.sh -s dir -t rpm ./build/sqoop-1.4.7=/opt
+fpm --verbose --maintainer support@altiscale.com --vendor Altiscale --provides alti-sqoop-${SQOOP_VERSION} -n alti-sqoop-${SQOOP_VERSION} -v ${ALTISCALE_RELEASE} --iteration ${DATE_STRING} --license "Apache License v2" --after-install create_link.sh -s dir -t rpm ./build/sqoop-${SQOOP_VERSION}=/opt
